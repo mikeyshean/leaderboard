0269f590f9cb079025fd66d4e800c52724830f5c
Mikey Shean <mdshean2@gmail.com>
2015-10-05 23:44:02 -0400

    Update origins.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Leaderboard
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'localhost:3000', '23.235.46.133'
+        origins 'localhost:3000', 'mikeyshean.com', 'http://www.mikeyshean.com'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
